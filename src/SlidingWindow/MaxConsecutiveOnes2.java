c48506421f8395ab3f9a45ac6ef32c5c95791d43
arpitp8595 <arpitp8595@gmail.com>
2025-05-19 23:06:57 -0400

    minor code-refactoring

--- src/SlidingWindow/MaxConsecutiveOnes2.java
+++ src/SlidingWindow/MaxConsecutiveOnes2.java
@@ -5,7 +5,8 @@ public class MaxConsecutiveOnes2 {
         int[] nums = {1,1,1,0,0,0,1,1,1,1,0}; int k = 2;
         System.out.println("Result: "+longestOnes(nums, k));
     }
-//Given a binary array nums and an integer k, return the maximum number of consecutive 1's in the array if you can flip at most k 0's.
+//Given a binary array nums and an integer k, return the maximum number of consecutive 1's in the array
+// if you can flip at most k 0's.
     public static int longestOnes(int[] nums, int k) {
         int left = 0, maxLength = 0, zeroCount = 0;
         for (int right = 0; right < nums.length; ++right) {
